153227ca9e22698a762c0b62f6a8233b143bf62e
Khaeruman1303 <muhammadkhaeruman1303@gmail.com>
2024-06-11 10:20:30 +0700

    tambah file khaeruman

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+if (!isset($_SESSION["login"])) {
+    header("Location: login.php");
+    exit;
+}
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+// tombol cari ditekan
+if (isset($_POST["cari"])) {
+    $mahasiswa = cari($_POST["keyword"]);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="js/script.js"></script>
+</head>
+
+<body>
+
+    <a href="logout.php">Logout</a>
+
+    <h1>Daftar Mahasiswa</h1>
+    <a href="tambah.php">Tambah data mahasiswa</a>
+    <br><br>
+
+    <form action="" method="post">
+
+        <input type="text" name="keyword" id="keyword" size="40" autofocus placeholder="masukkan keyword pencarian" autocomplete="off">
+        <button type="submit" name="cari" id="tombol-cari">Cari!</button>
+
+    </form>
+
+    <br>
+    <div id="container">
+        <table border="1" cellpadding="10" cellspacing="0">
+
+            <tr>
+                <th>No.</th>
+                <th>Aksi</th>
+                <th>NRP</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Jurusan</th>
+                <th>Gambar</th>
+            </tr>
+
+            <?php $i = 1; ?>
+            <?php foreach ($mahasiswa as $row) : ?>
+                <tr>
+                    <td><?= $i; ?></td>
+                    <td>
+                        <a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a> |
+                        <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('Yakin?');">hapus</a>
+                    </td>
+                    <td><?= $row["nrp"]; ?></td>
+                    <td><?= $row["nama"]; ?></td>
+                    <td><?= $row["email"]; ?></td>
+                    <td><?= $row["jurusan"]; ?></td>
+                    <td><img src="img/<?= $row["gambar"]; ?>" alt="" width="60"></td>
+                </tr>
+                <?php $i++; ?>
+            <?php endforeach; ?>
+
+        </table>
+    </div>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,106 @@
+<?php
+session_start();
+require 'functions.php';
+
+// cek cookie
+if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
+    $id = $_COOKIE['id'];
+    $key = $_COOKIE['key'];
+
+    // ambil username berdasarkan id
+    $result = mysqli_query($conn, "SELECT username FROM user WHERE id = $id");
+    $row = mysqli_fetch_assoc($result);
+
+    // cek cookie dan username
+    if ($key === hash('sha256', $row['username'])) {
+        $_SESSION['login'] = true;
+    }
+}
+
+if (isset($_SESSION["login"])) {
+    header("Location: index.php");
+    exit;
+}
+
+if (isset($_POST["login"])) {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+
+    // cek username
+    if (mysqli_num_rows($result) === 1) {
+        // cek password
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row["password"])) {
+            // set session
+            $_SESSION["login"] = true;
+
+            // cek remember me
+            if (isset($_POST["remember"])) {
+                // buat cookie
+                setcookie('id', $row['id'], time() + 60);
+                setcookie('key', hash('sha256', $row['username']), time() + 60);
+            }
+
+            header("Location: index.php");
+            exit;
+        }
+    }
+
+    $error = true;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container text-center p-5">
+        <h1>Halaman Login</h1>
+
+        <?php if (isset($error)) : ?>
+            <p style="color: red; font-style: italic;">username / password salah</p>
+        <?php endif; ?>
+
+        <form action="" method="post">
+
+            <ul class="list-unstyled">
+                <li>
+                    <label for="username">Username : </label>
+                    <input type="text" name="username" id="username">
+                </li>
+                <li>
+                    <label for="password">Password : </label>
+                    <input type="password" name="password" id="password">
+                </li>
+                <li>
+                    <input type="checkbox" name="remember" id="remember">
+                    <label for="remember">Remember me</label>
+                </li>
+                <li>
+                    <button type="submit" name="login">Login!</button>
+                </li>
+            </ul>
+
+        </form>
+    </div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
