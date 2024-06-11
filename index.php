1997026c987182f481cb86d48b0c93ab1a0f6522
Khaeruman1303 <muhammadkhaeruman1303@gmail.com>
2024-06-11 23:57:57 +0700

    buat halaman awal dan tambah gambar,

--- index.php
+++ index.php
@@ -1,77 +1,277 @@
-<?php
-session_start();
-if (!isset($_SESSION["login"])) {
-    header("Location: login.php");
-    exit;
-}
-require 'functions.php';
-$mahasiswa = query("SELECT * FROM mahasiswa");
-
-// tombol cari ditekan
-if (isset($_POST["cari"])) {
-    $mahasiswa = cari($_POST["keyword"]);
-}
-
-?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Admin</title>
-    <script src="js/jquery-3.7.1.min.js"></script>
-    <script src="js/script.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bimbel Tryout CPNS, BUMN, Kedinasan, PPPK</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
 <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-light border-bottom">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="img/logo.png" alt="Bootstrap" width="30" height="40">
+            </a>
+            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button> -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-evenly flex-grow-1">
+                        <li class="nav-item me-5">
+                            <a class="nav-link fs-5" href="#">Beranda</a>
+                        </li>
+                        <li class="nav-item me-5">
+                            <a class="nav-link fs-5" href="#">Produk Kami</a>
+                        </li>
+                        <li class="nav-item me-5">
+                            <a class="nav-link fs-5" href="#">Keunggulan</a>
+                        </li>
+                        <li class="nav-item me-5">
+                            <a class="nav-link fs-5" href="#">Testimoni</a>
+                        </li>
+                        <li class="nav-item me-5">
+                            <a class="nav-link fs-5" href="#">Pilihan Paket</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link btn btn-success text-white fs-5">Masuk</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+    </nav>
+    <!-- Akhir Navbar -->
 
-    <a href="logout.php">Logout</a>
+    <!-- Highlight -->
+    <section>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-7 text-start">
+                    <p class="fs-1 fw-bold">Bergabung Bersama<br>
+                        Peserta Lainnya Untuk<br>
+                        Persiapkan Diri<br>
+                        Menghadapi Ujian</p>
+                    <div class="grid text-start">
+                        <div class="g-col btn btn-success fw-bold fs-5" href="#">Gabung Sekarang</div>
+                        <div class="g-col btn btn-success fw-bold fs-5" href="#">Masuk</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir Highlight -->
 
-    <h1>Daftar Mahasiswa</h1>
-    <a href="tambah.php">Tambah data mahasiswa</a>
-    <br><br>
+    <!-- Tentang -->
+    <section id="tentang">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-5 text-center">
 
-    <form action="" method="post">
+                </div>
+                <div class="col-md-7 text-start">
+                    <p class="fs-1 fw-bold">Tentang Kami</p>
+                    <p class="fs-5">Adalah website layanan belajar dan tryout CPNS dan Sekolah Kedinasan untuk persiapan tes CPNS dan Sekolah Kedinasan.<br><br>
+                        juga tersedia informasi, materi, soal, dan tryout online CPNS dan Sekolah Kedinasan gratis untuk mempersiapkan diri dalam menghadapi seleksi CPNS dan Sekolah Kedinasan.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir Tentang -->
 
-        <input type="text" name="keyword" id="keyword" size="40" autofocus placeholder="masukkan keyword pencarian" autocomplete="off">
-        <button type="submit" name="cari" id="tombol-cari">Cari!</button>
+    <!-- Produk -->
+    <section id="produk">
+        <div class="container mt-5">
+            <div class="row text-center mb-3">
+                <div class="col" id="produk">
+                    <h2 class="fs-1 fw-bold">Produk Kami</h2>
+                </div>
+            </div>
+            <div class="row g-4">
+                <div class="col-md-4">
+                    <div class="shadow bg-light border border-white border-5 rounded p-4">
+                        <h1 class="fw-bold">Tryout Gratis</h1>
+                        <p class="fs-5">Ikuti tryout gratis setiap minggu serentak bersama peserta di seluruh Indonesia</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="shadow bg-light border border-white border-5 rounded p-4">
+                        <h1 class="fw-bold">Tryout Premium</h1>
+                        <p class="fs-5">Asah kemampuan Anda dengan tryout premium yang menyediakan fitur-fitur lengkap seperti ranking, pembahasan, grafik analisa, dll yang dapat membantu mengasah kemampuan kamu lebih dalam lagi</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="shadow bg-light border border-white border-5 rounded p-4">
+                        <h1 class="fw-bold">Bimbel</h1>
+                        <p class="fs-5">Ingin belajar lebih intensif lagi? Ikuti kelas bimbel dengan mentor-mentor ahli dibidangnya dan terbukti telah lulus menjadi ASN.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir Produk -->
 
-    </form>
+    <!-- Keunggulan -->
+    <section id="keunggulan">
+        <div class="container mt-5">
+            <div class="row text-center mb-3">
+                <div class="col">
+                    <h2 class="fs-1 fw-bold">Keunggulan</h2>
+                </div>
+            </div>
+            <div class="bg-success pt-5 text-white border border-5 rounded border-white shadow">
+                <div class="row text-center justify-content-evenly mb-3">
+                    <div class="col-md-3">
+                        <h2 class="fw-bold mb-3">Tryout Gratis</h2>
+                        <p class="fs-5">Tryout CPNS dan Sekolah Kedinasan online setiap minggu yang dapat Anda ikuti secara gratis.</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h2 class="fw-bold mb-3">Hasil instan</h2>
+                        <p class="fs-5">Hasil ujian dapat langsung dilihat setelah peserta selesai mengerjakan paket tryout CPNS / Sekolah Kedinasan.</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h2 class="fw-bold mb-3">Peringkat Nasional</h2>
+                        <p class="fs-5">Peserta dapat melihat peringkat antar peserta seluruh Indonesia.</p>
+                    </div>
+                </div>
+                <div class="row text-center justify-content-evenly">
+                    <div class="col-md-3">
+                        <h2 class="fw-bold mb-3">Fitur Lengkap</h2>
+                        <p class="fs-5">Fitur-fitur yang kami berikan akan dapat membantu Anda dalam mengasah kemampuan untuk menghadapi seleksi CPNS dan Sekolah Kedinasan yang akan datang.</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h2 class="fw-bold mb-3">Bimbel Terstruktur</h2>
+                        <p class="fs-5">Bimbel IDCPNS disusun seefektif mungkin agar materi-materi yang dipelajari sangat terarah sesaui kisi-kisi yang akan diujikan.</p>
+                    </div>
+                    <div class="col-md-3">
+                        <h2 class="fw-bold mb-3">Soal Terbaru</h2>
+                        <p class="fs-5">Paket soal CPNS dan Sekolah Kedinasan yang kami berikan selalu diperbaharui sesuai kisi-kisi yang diberikan pemerintah, sehingga dapat meningkatkan kemampuan peserta secara optimal.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir Keunggulan -->
 
-    <br>
-    <div id="container">
-        <table border="1" cellpadding="10" cellspacing="0">
+    <!-- Testimoni -->
 
-            <tr>
-                <th>No.</th>
-                <th>Aksi</th>
-                <th>NRP</th>
-                <th>Nama</th>
-                <th>Email</th>
-                <th>Jurusan</th>
-                <th>Gambar</th>
-            </tr>
+    <!-- Akhir Testimoni -->
 
-            <?php $i = 1; ?>
-            <?php foreach ($mahasiswa as $row) : ?>
-                <tr>
-                    <td><?= $i; ?></td>
-                    <td>
-                        <a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a> |
-                        <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('Yakin?');">hapus</a>
-                    </td>
-                    <td><?= $row["nrp"]; ?></td>
-                    <td><?= $row["nama"]; ?></td>
-                    <td><?= $row["email"]; ?></td>
-                    <td><?= $row["jurusan"]; ?></td>
-                    <td><img src="img/<?= $row["gambar"]; ?>" alt="" width="60"></td>
-                </tr>
-                <?php $i++; ?>
-            <?php endforeach; ?>
+    <!-- Paket -->
+    <section>
+        <div class="container mt-5">
+            <div class="row text-center mb-3">
+                <div class="col" id="produk">
+                    <h2 class="fs-1 fw-bold">Pilihan Paket</h2>
+                </div>
+            </div>
+            <div class="row g-4">
+                <div class="col-md-6">
+                    <div class="shadow bg-light border border-white border-5 rounded p-4">
+                        <p class="fs-5">Tryout Gratis</p>
+                        <h1 class="fw-bold mb-4">Gratis</h1>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Sistem CAT/CBT</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Timer Ujian</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Nilai Langsung Keluar</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-x-square-fill text-danger"></i> Perankingan Nasional,Provinsi, Instansi</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-x-square-fill text-danger"></i> Grafik Evaluasi</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-x-square-fill text-danger"></i> Review Pembahasan</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-x-square-fill text-danger"></i> Dikerjakan Berulang Kali</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-x-square-fill text-danger"></i> Tidak Perlu Repost / Comment Post IG</div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="shadow bg-light border border-white border-5 rounded p-4">
+                        <p class="fs-5">Tryout Premium</p>
+                        <h1 class="fw-bold mb-4">Rp 30.000</h1>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Sistem CAT/CBT</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Timer Ujian</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Nilai Langsung Keluar</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Perankingan Nasional,Provinsi, Instansi</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Grafik Evaluasi</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Review Pembahasan</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Dikerjakan Berulang Kali</div>
+                        <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Tidak Perlu Repost / Comment Post IG</div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="shadow bg-light border border-white border-5 rounded p-4">
+                        <div class="col-md-4">
+                            <p class="fs-5">Bimbel</p>
+                            <h1 class="fw-bold mb-4">Rp 199.000</h1>
+                            <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> 13x Pertemuan Live Via Zoom</div>
+                            <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> 3 Paket Tryout Premium</div>
+                            <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Channel Mentoring</div>
+                            <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Tersedia Rekaman</div>
+                            <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Menggunakan Ketentuan Terbaru</div>
+                            <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Fitur Tryout Terlengkap</div>
+                            <div class="container bg-white p-3 fs-5 mb-3"><i class="bi bi-check-square-fill text-success"></i> Penilaian, Pembahasan Ranking Nasional</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir Paket -->
 
-        </table>
-    </div>
+    <!-- Footer -->
+    <section>
+        <div class="container-fluid bg-dark mt-5">
+            <div class="container">
+                <div class="row pt-5 pb-5">
+                    <div class="col-md-6">
+                        <img src="img/logoputih.png" alt="" width="120" height="130">
+                        <p class="fs-5 text-white mt-4">Adalah website layanan belajar dan tryout CPNS dan Sekolah Kedinasan untuk persiapan tes CPNS dan Sekolah Kedinasan. Di sini juga tersedia informasi, materi, soal, dan tryout online CPNS dan Sekolah Kedinasan gratis untuk mempersiapkan diri dalam menghadapi seleksi CPNS dan Sekolah Kedinasan</p>
+                    </div>
+                    <div class="col-md-3 text-white">
+                        <h2 class="fw-bold mb-3">Lebih Lengkap</h2>
+                        <a href="#" style="text-decoration: none;">
+                            <p class="fs-5 text-white mt-4">Blog</p>
+                        </a>
+                        <a href="#" style="text-decoration: none;">
+                            <p class="fs-5 text-white mt-4">Sitemap</p>
+                        </a>
+                        <a href="#" style="text-decoration: none;">
+                            <p class="fs-5 text-white mt-4">Syarat dan Ketentuan</p>
+                        </a>
+                        <a href="#" style="text-decoration: none;">
+                            <p class="fs-5 text-white mt-4">Kebijakan Privasi</p>
+                        </a>
+                        <a href="#" style="text-decoration: none;">
+                            <p class="fs-5 text-white mt-4">Kebijakan Pengembalian Dana</p>
+                        </a>
+                        <a href="#" style="text-decoration: none;">
+                            <p class="fs-5 text-white mt-4">Kontak Kami</p>
+                        </a>
+                        <a href="#" style="text-decoration: none;">
+                            <p class="fs-5 text-white mt-4">FAQ</p>
+                        </a>
+                    </div>
+                    <div class="col-md-3 text-white">
+                        <h2 class="fw-bold mb-3">Find Us On</h2>
+                        <p class="fs-1"><i class="bi bi-tiktok text-white"></i></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid text-center bg-black pt-4 pb-2">
+            <p class="fs-5 text-white">Education Squad &copy 2024</p>
+        </div>
+    </section>
+    <!-- Akhir Footer -->
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
