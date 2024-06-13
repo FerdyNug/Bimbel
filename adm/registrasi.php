0be26b1524ad1b4d5ef5a231dc2fffed854acfd9
Khaeruman1303 <muhammadkhaeruman1303@gmail.com>
2024-06-13 07:27:13 +0700

    tambah halaman registrasi

--- adm/registrasi.php
+++ adm/registrasi.php
@@ -20,42 +20,61 @@ if (isset($_POST["register"])) {
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="" />
+    <meta name="author" content="" />
     <title>Halaman Registrasi</title>
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/custom.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <style>
-        label {
-            display: block;
-        }
-    </style>
 </head>
 
-<body>
-    <div class="container text-center p-5">
-        <h1>Halaman Registrasi</h1>
-
-        <form action="" method="post">
-
-            <ul class="list-unstyled">
-                <li>
-                    <label for="username">username : </label>
-                    <input type="text" name="username" id="username">
-                </li>
-                <li>
-                    <label for="password">password : </label>
-                    <input type="password" name="password" id="password">
-                </li>
-                <li>
-                    <label for="password2">konfirmasi password : </label>
-                    <input type="password" name="password2" id="password2">
-                </li>
-                <li>
-                    <button type="submit" name="register">Register!</button>
-                </li>
-            </ul>
-
-        </form>
+<body class="bodyLogin">
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-5">
+                            <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                <div class="card-header">
+                                    <h3 class="text-center font-weight-light my-4 fw-bold">Halaman Registrasi <br> Admin</h3>
+                                </div>
+                                <div class="card-body">
+                                    <?php if (isset($error)) : ?>
+                                        <p style="color: red; font-style: italic;">username / password salah</p>
+                                    <?php endif; ?>
+                                    <form action="" method="post">
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" type="text" name="username" id="username">
+                                            <label for="username">Username</label>
+                                        </div>
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" type="password" name="password" id="password">
+                                            <label for="password">Password</label>
+                                        </div>
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" type="password" name="password2" id="password2">
+                                            <label for="password2">Konfirmasi Password : </label>
+                                        </div>
+                                        <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                            <button class="btn btn-primary" type="submit" name="register">Register!</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
     </div>
+
+    <?php
+    require('footer.php');
+    ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
