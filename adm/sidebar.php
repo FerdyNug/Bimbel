bcc9387be4ab15fd17bb327011e23107194eceec
FerdyNug <nugrahaferdy68@gmail.com>
2024-06-15 17:06:07 +0700

    sidebar

--- adm/sidebar.php
+++ adm/sidebar.php
@@ -16,10 +16,15 @@
                     </a>
                     <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
                         <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                            <a class="nav-link" href="landpage.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-reguler fa-window-maximize"></i></div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
                                 Landing Page
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                             </a>
+                            <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="banner.php"><div class="sb-nav-link-icon"><i class="fas fa-reguler fa-window-maximize"></i></div>Banner</a>
+                                </nav>
+                            </div>
                             <a class="nav-link" href="tryout.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-solid fa-file-pen"></i></div>
                                 Tryout
